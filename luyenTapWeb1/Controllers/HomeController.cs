c53400d820424237d24cb27a223b25d8e9e2ef28
huukhanh2603 <147917702+huukhanh2603@users.noreply.github.com>
2024-11-06 13:48:16 +0700

    thêm dự án

---
+++ luyenTapWeb1/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+using luyenTapWeb1.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace luyenTapWeb1.Controllers
+{
+    public class HomeController : Controller
+    {
+        QlbanVaLiContext db = new QlbanVaLiContext();
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            var lstsanpham = db.TDanhMucSps.ToList();
+
+            return View(lstsanpham);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ luyenTapWeb1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace luyenTapWeb1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
